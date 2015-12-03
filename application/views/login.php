4597b989307f88e75e4824722b5d549753aaf17d
Aluno <iago-olvr>
2015-12-03 19:42:30 -0200

    criação CORETA do login.

---
+++ application/views/login.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Dynado | Login</title>
+
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet">
+
+    <link href="assets/css/animate.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body class="gray-bg">
+
+<div class="middle-box text-center loginscreen  animated fadeInDown">
+    <div>
+        <div>
+
+            <h1 class="logo-name">DF</h1>
+
+        </div>
+        <h3>Bem vindo ao Dyando Finanças</h3>
+        <p>
+            Um lugar para controlar seus gastos de forma efetiva e administrar o seu dinheiro.
+            <!--Continually expanded and constantly improved Inspinia Admin Them (IN+)-->
+        </p>
+        <p>Começe agora.</p>
+        <form class="m-t" role="form" action="index.html">
+            <div class="form-group">
+                <input type="email" class="form-control" placeholder="Nome" required="">
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" placeholder="Senha" required="">
+            </div>
+            <button type="submit" class="btn btn-primary block full-width m-b">Entrar</button>
+
+            <a href="#"><small>Esqueci minha senha.</small></a>
+            <p class="text-muted text-center"><small>Não tem conta?</small></p>
+            <a class="btn btn-sm btn-white btn-block" href="register.html">Cadastre-se</a>
+        </form>
+        <p class="m-t"> <small>Dynado Finanças &copy; 2015</small> </p>
+    </div>
+</div>
+
+<!-- Mainly scripts -->
+<script src="js/jquery-2.1.1.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
