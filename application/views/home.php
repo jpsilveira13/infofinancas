a0520ea57c2e2f2db69b891c78a73094f5f2cd72
Iago <418510>
2015-12-03 15:54:14 -0200

    criei duas views ( login , register) , mudei no controlador a home pra login mas ainda não liguei as páginas.

--- application/views/home.php
+++ application/views/home.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>INSPINIA | Dashboard</title>
+    <title>Dynado Finanças</title>
 
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet">
