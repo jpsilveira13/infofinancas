69656cb9eb6df164d6d3fecb34e178685a766177
Iago <418510>
2015-12-10 02:19:38 -0200

    sono

--- application/controllers/ProfileController.php
+++ application/controllers/ProfileController.php
@@ -17,4 +17,23 @@ class ProfileController extends CI_Controller
         $this->load->view('profile');//chama a tela do perfil
 
     }
+
+    public function upload()
+    {
+        $config['upload_path'] = './upload/';
+        $config['allowed_types'] = 'gif|jpg|png';
+        $config['max_size']	= '100';
+        $config['max_width']  = '1024';
+        $config['max_height']  = '768';
+
+        $this->load->library('upload', $config);
+
+        if (  $this->upload->do_upload())
+        {
+            $data = array('upload_data' => $this->upload->data());
+
+            $this->load->view('upload_success', $data);
+        }
+
+    }
 }
